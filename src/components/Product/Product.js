734bb78d9565393142f460b18cdd487c805871ce
DmitriX <dodge.241294@gmail.com>
2024-02-22 15:21:29 +0300

    add: Product card was added

---
+++ src/components/Product/Product.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Product.module.css';
+
+export const Product = ({id, price, brand, product: title}) => {
+    return (
+        <div className={styles.card}>
+            <dl className={styles['info-list']}>
+                <dt></dt>
+                <dd className={styles.title}>{title}</dd>
+
+                <dt></dt>
+                <dd className={styles.price}>$ {new Intl.NumberFormat('en-US').format(price)}</dd>
+
+                <dt className={styles['prop-name']}>ID:</dt>
+                <dd className={styles['prop-value']}>{id}</dd>
+
+                <dt className={styles['prop-name']}>Бренд:</dt>
+                <dd className={styles['prop-value']}>{brand}</dd>
+
+            </dl>
+        </div>
+    );
+}
