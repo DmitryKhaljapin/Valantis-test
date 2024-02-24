18aef9d2d21adf1deeb6abd1e29d1da51cbfa82a
DmitriX <dodge.241294@gmail.com>
2024-02-24 11:15:13 +0300

    add: loading icon was added

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Product } from '../Product/Product';
 import { getProducts } from './getProducts';
+import { Loading } from '../Loading/Loading';
 import { ControlBar } from '../UI/ControlBar/ControlBar';
 import styles from './Products.module.css';
 
 export const Products = () => {
 
     const [productsList, setProductsList] = useState(null)
-    const [pageNumber, setPageNumber] = useState(161);
+    const [pageNumber, setPageNumber] = useState(1);
 
     function showNextPage() {
         setPageNumber(prevState => prevState + 1);
@@ -21,17 +22,20 @@ export const Products = () => {
         async function getData() {
             setProductsList(await getProducts(pageNumber));
         }
+        setProductsList(null);
         getData();
     }, [pageNumber]);
 
     return (
         <div className={styles['products-container']}>
-            {productsList && <>
+            {productsList 
+            ?<>
                 <ul>
                     {productsList.uniqProductsList.map(productItem => <li key={productItem.id}><Product  id={productItem.id} brand={productItem.brand} product={productItem.product} price={productItem.price} /></li>)}
                 </ul>
                 <ControlBar showNextPage={showNextPage} showPreviousPage={showPreviousPage} pageNumber={pageNumber} isLastPage={productsList.isLastPage} />
-            </>}
+            </>
+            :<Loading />}
         </div>
     )
 }
