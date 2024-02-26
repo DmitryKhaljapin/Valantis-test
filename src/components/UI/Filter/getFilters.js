88cbd2e7f8f3d6e9d498fbd62fb151a9d9402eba
DmitriX <dodge.241294@gmail.com>
2024-02-26 10:06:51 +0300

    del: commented code was deleted

--- src/components/UI/Filter/getFilters.js
+++ src/components/UI/Filter/getFilters.js
@@ -1,25 +1,5 @@
 import { requireToServer } from '../../../helpers/requireToServer';
 
-// export async function getFilteredProducts() {
-//     // const productIds = await getFilteredProductsList({"product": 'Золотое кольцо'});
-//     // const productsList = await getProductsList(productIds.result)
-//     const fields = await getFields({field: 'brand'});
-
-// }
-
-
-// async function getFilteredProductsList(filterParameter) {
-//     return axios.post(url, {
-//         action: 'filter',
-//         params: filterParameter,
-//     }, headers)
-//     .then((response) => response.data)
-//     .catch(e => {
-//         if (e.message) console.log(e.message);
-//         return getFilteredProductsList(filterParameter)
-//     });
-// }
-
 async function getFilteringFields() {
     return requireToServer('get_fields');
 }
